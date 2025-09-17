7704cf0dc1f220643b002dbd3ba99af83868c4d4
minemobs <issahakim94@gmail.com>
2025-09-17 18:42:33 +0200

    Initial commit

---
+++ tests/parser.test.ts
@@ -0,0 +1,23 @@
+// @position layer=message1 page=fore color=0 opacity=255 frame="" visible=true left=0 top=0 width=&kag.scWidth height=&kag.scHeight marginL=0
+
+import { expect, test } from "bun:test";
+import { splitMacroArguments } from "..";
+
+test("parse position macro arguments", () => {
+  const args = splitMacroArguments(`@position layer=message1 page=fore color=0 opacity=255 frame="" visible=true left=0 top=0 width=&kag.scWidth height=&kag.scHeight marginL=0`);
+  console.log(Object.fromEntries(args));
+  expect(Object.fromEntries(args))
+    .toStrictEqual({
+        layer: "message1",
+        page: "fore",
+        color: "0",
+        opacity: "255",
+        frame: "",
+        visible: "true",
+        left: "0",
+        top: "0",
+        width: "&kag.scWidth",
+        height: "&kag.scHeight",
+        marginL: "0",
+      })
+});
